6664bc46b49c8125a5db5c6357dd1fb62189a5a2
Manoj Naidu <manojnaidu619@gmail.com>
2019-07-15 14:17:30 +0530

    Array push function added

--- src/components/AddQuote.vue
+++ src/components/AddQuote.vue
@@ -4,8 +4,8 @@
       <div class="col-6">
         <form>
           <div class="form-group">
-            <textarea class="form-control" placeholder="Add a Quote" rows="3"></textarea><br>
-            <button type="button" class="btn btn-success btn-large" name="button">Add Quote</button>
+            <textarea class="form-control" placeholder="Add a Quote" rows="3" v-model='newQuote'></textarea><br>
+            <button type="button" class="btn btn-success btn-large" @click = "PushQuote()" name="button">Add Quote</button>
           </div>
         </form>
       </div>
@@ -14,7 +14,22 @@
 </template>
 
 <script>
-
+ export default{
+     props: ['quotes'],
+     data(){
+       return {
+         newQuote: ""
+       }
+     },
+     methods:{
+       PushQuote(){
+         if(this.newQuote.length > 3){
+           this.quotes.push(this.newQuote)
+           this.newQuote = ""
+         }
+       }
+     }
+ }
 </script>
 
 <style>
